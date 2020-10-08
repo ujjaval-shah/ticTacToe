879d9143b52c66bb4b1ed9d1fa4b9bbe7d665dd4
ujjaval-shah <uss1999int@gmail.com>
2020-10-08 11:01:50 +0530

    Updates

---
+++ precache-manifest.ee22ec4f79fb8824fa66692bac64b920.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "239ff29d0e7691c2985c3c3bbd46c9ad",
+    "url": "/ticTacToe/index.html"
+  },
+  {
+    "revision": "d156e70247a5a13a9663",
+    "url": "/ticTacToe/static/css/main.708bc36f.chunk.css"
+  },
+  {
+    "revision": "c123e1373892c8f0a0b2",
+    "url": "/ticTacToe/static/js/2.4d81bbee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ticTacToe/static/js/2.4d81bbee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d156e70247a5a13a9663",
+    "url": "/ticTacToe/static/js/main.106b060a.chunk.js"
+  },
+  {
+    "revision": "58614dcbc8bd496cd5a7",
+    "url": "/ticTacToe/static/js/runtime-main.3f686040.js"
+  }
+]);
